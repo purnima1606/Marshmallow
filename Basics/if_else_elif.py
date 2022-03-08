5a982e1e33fd7ff7701a81594a8609093b832da0
purnima1606 <purnimasingh26th@gmail.com>
2022-03-09 03:42:54 +0530

    1st commit

---
+++ Basics/if_else_elif.py
@@ -0,0 +1,137 @@
+# a = 3
+# if a > 2:
+#     print("enter the block")
+
+# 1 wap to check whether number is even or odd:
+# a=4
+# if a % 2 == 0:
+#     print(f"{a} is an even number: ")
+# else:
+#     print(f"{a} is an odd number")
+
+# 2 wap to check whether the string/iterable even length or not :
+# s = "hello"
+# if len(s) % 2 == 0:    # bool(condition)
+#     print("iterable has even number of elements:")
+# else:
+#     print("iterable has odd number of elements:")
+
+# 3 wap to check whether the given string is palindrome or not :
+# s = "python"
+# if s == s[::-1]:
+#     print("given string is palindrome:")
+# else:
+#     print("given string is not palindrome")
+
+# 4 wap to check whether the given number is palindrome or not :
+# n = 1221
+# x = str(n)
+# if x == x[::-1]:
+#     print("given number is palindrome:")
+# else:
+#     print("given number is not palindrome:")
+
+# 5 wap to check whether the string is starting with vowel or not :
+# s = "apple"
+# if s[0] in "AEIOUaeiou":
+#     print("string is starting with vowel:")
+# else:
+#     print("string is not starting with vowel:")
+
+# 6 wap to check whether the string is ending with vowel or not :
+# s = "hello"
+# if s[-1] in "aeiouAEIOU":
+#     print("string is ending with vowel :")
+# else:
+#     print("string is not ending with vowel :")
+
+# 7 wap to check whether the given iterable is empty or not :
+# s="hai"
+# if s:                  # if bool(bool(s)):  true/1
+#     print("string is not empty :")
+# else:
+#     print("string is empty :")
+
+# 8 wap to check if the first digit of the number is divisible by 3 or not :
+# s = 96543
+# x = str(s)
+# if int(x[0]) % 3 == 0:
+#     print("divisible by 3:")
+# else:
+#     print("not divisible :")
+
+# 9 wap to print greatest of 3 number :
+# a = 10
+# b = 60
+# c = 100
+# if (a > b) and (a > c):
+#     print(f"{a} is greatest number :")
+# elif (b > c):
+#     print(f"{b} is greatest number :")
+# else:
+#     print(f"{c} is greatest number : ")
+
+
+
+"""        ELIF CONDITIONAL STATEMENT        """
+
+# 1 wap to check if the given character is an alphabet or digit or special character without using built in method :
+# c = "h"
+# if "a"<= c <= "z" or "A" <= c <= "Z":
+#     print("character is an alphabet :")
+# elif "0"<= c<= "9":
+#     print("character is an digit :")
+# else:
+#     print("character is special symbol :")
+
+# 2 wap to convert lower case character to upper case character and vise versa :
+
+# 3 wap to check if the given string is starting with consonent or not :
+# 4 wap to check if the given string is starting with vowel create a dictionary with string is key and it is length is value :
+""" 5 wap to check  if the string /iterable is off even length or not, if it is off even length then create a 
+ dictionary with string as key and ASCII value off it's first character as 
+its value if the string is off the odd length and string as key and length of the string is value : """
+
+"""        INLINE CONDITIONAL STATEMENT          """
+# wap to check whether number is even or odd:
+# a = 4
+# print("even"if a % 2 == 0 else "odd number")
+
+# wap to check whether the string/iterable even length or not :
+# s = "hello"
+# print("iterable has even number of elements:" if len(s) % 2 == 0 else "iterable has odd number of elements:")
+
+# 3 wap to check whether the given string is palindrome or not :
+# s = "mom"
+# print("palindrome :" if s==s[::-1] else "not palindrome :")
+
+# 4 wap to check whether the given number is palindrome or not :
+# s = 1221
+# x=str(s)
+# print("palindrome number :" if x == x[::-1] else "not palindrome :")
+
+# 5 wap to check whether the string is starting with vowel or not :
+# s = "hello"
+# print("starting with vowel" if s[0] in "aeiouAEIOU" else "not starting with vowel :" )
+
+# 6 wap to check whether the string is ending with vowel or not :
+# s = "hello"
+# print("ending with vowel" if s[-1] in "aeiouAEIOU" else "not ending with vowel :")
+
+# 7 wap to check whether the given iterable is empty or not :
+# s = "hai"
+# print("iterable is not empty :" if s else "iterable is empty :")
+
+# 8 wap to check if the first digit of the number is divisible by 3 or not :
+# s=1234
+# x=str(s)
+# print("divisible by 3 :" if int(x[0])%3==0 else "not divisible by 3 :")
+
+
+
+
+
+
+
+
+
