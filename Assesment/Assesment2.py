30893b744c9d33703b6a24cf70604e744bbc132f
purnima1606 <purnimasingh26th@gmail.com>
2022-07-14 00:14:26 +0530

    1st commit

--- Assesment/Assesment2.py
+++ Assesment/Assesment2.py
@@ -1,9 +1,10 @@
 """ 1. WAP to create a dictionary with character and its count pair from the given string """
-s = "hello world"
-
+# s = "hello world"
+# d = {i:s.count(i)for i in set(s)}
+# print(d)     # {'o': 2, 'l': 3, 'd': 1, 'r': 1, ' ': 1, 'w': 1, 'h': 1, 'e': 1}
 
 """2. WAP to print nth Fibonacci number"""
-
+# n = int(input())
 
 """ 3. WAP to count the number of occurrence  """
 s ='hello@world! welcome!!! Python$ hi how are you & where are you?'
@@ -11,17 +12,24 @@ s ='hello@world! welcome!!! Python$ hi how are you & where are you?'
 """ 4. WAP to iterate through list and built a new dictionary , only if the items of the list has even number of 
 characters """
 # names = ['apple', 'yahoo', 'google', 'gmail', 'walmart', 'flipkart', 'facebook', 'amazon']
-
+# d = {i:names.count(i) for i in set(names) if len(i)%2 == 0}
+# print(d)    # {'flipkart': 1, 'amazon': 1, 'google': 1, 'facebook': 1}
 
 """ 5. WAP to find the third largest number in the list without using any inbuilt functions"""
 # numbers = [10, 20, 30, 40, 50]
 
 """ 6. WAP to print all numeric values in a list """
 # items = ['apple', 1.2, 'google', '12.6', 26, '100']
+#
+# for i in items:
+#     if isinstance(i, (int, float, complex)):
+#         print(i)  # 1.2  26
 
 
 """ 7. WAP to create a dictionary of element and element ot the power of its index pair """
 # a = [1, 2, 3, 4, 5]
+# d = {item:item ** index for index, item in enumerate(a)}
+# print(d) # {1: 1, 2: 2, 3: 9, 4: 64, 5: 625}
 
 """ 8. WAP to create a dictionary item and number of occurrences of each item in the list without using any inbuilt 
 functions"""
