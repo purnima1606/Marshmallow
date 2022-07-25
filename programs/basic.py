e68c32d462cb5cbe1695c98fba0f86b639798209
purnima1606 <purnimasingh26th@gmail.com>
2022-07-25 20:14:46 +0530

    1st commit

---
+++ programs/basic.py
@@ -0,0 +1,33 @@
+                                  # Basic Programs:
+
+""" Python program to add two numbers"""
+# a = 10
+# b = 20
+# a = int(input("enter the value of a"))
+# b = int(input("enter the value of b"))
+# print(a+b)
+
+""" Maximum of two numbers in Python  """
+# a = int(input("enter the value of a:"))
+# b = int(input("enter the value of b:"))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+"""Python Program for factorial of a number """
+
+
+"""Python Program for simple interest
+Python Program for compound interest
+Python Program to check Armstrong Number
+Python Program for Program to find area of a circle
+Python program to print all Prime numbers in an Interval
+Python program to check whether a number is Prime or not
+Python Program for n-th Fibonacci number
+Python Program for How to check if a given number is Fibonacci number?
+Python Program for n\’th multiple of a number in Fibonacci Series
+Program to print ASCII Value of a character
+Python Program for Sum of squares of first n natural numbers
+Python Program for cube sum of first n natural numbers
+ """
