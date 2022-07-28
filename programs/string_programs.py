384f3e2803fde82853771afd33a742dc179ed50f
purnima1606 <purnimasingh26th@gmail.com>
2022-07-28 23:35:54 +0530

    1st commit

--- programs/string_programs.py
+++ programs/string_programs.py
@@ -19,6 +19,7 @@
 #     print("string is not palindrome:")
 
 """ Python program to check whether the string is Symmetrical or Palindrome"""
+#  same answer
 
 """ Reverse words in a given String in Python """
 
@@ -28,13 +29,42 @@
 #     l.append(i[::-1])
 # print(" ".join(l))
 
-""" Ways to remove i’th character from string in Python"""
+""" Ways to remove i’th character from string in Python """
 
-""" Python | Check if a Substring is Present in a Given String"""
+# n = int(input("enter nth position:"))
+# s = "python is easy language"
+# res = ""
+# for index,i in enumerate(s):
+#     if index != n:
+#         res += i
+#     else:
+#         continue
+#
+# print(res)
+
+""" Python | Check if a Substring is Present in a Given String """
+
+# s = "hi hello you mad"
+# n = input("enter substring:")
+# if n in s:
+#     print(f"substring {n} is present in {s}:")
+# else:
+#     print(f"substring {n} is not present in {s}:")
 
 """ Python – Words Frequency in String Shorthands """
 
+# s = "are you looking for job if yes then you are selected no interview no tension"
+# for word in set(s.split()):
+#     print(word, s.count(word), end="  ")
+
+
 """ Python – Convert Snake case to Pascal case """
+# snake case = geeks_for_geeks
+# pascal case = GeeksforGeeks
+
+# s = "geeks_for_geeks"
+# res = s.title()
+# print("".join(res.split("_")))
 
 """ Find length of a string in python (4 ways) """
 
@@ -73,30 +103,68 @@
 #     print("the strings not contains all vowels")
 
 """ Python | Count the Number of matching characters in a pair of string """
+# s = "python is programing language"
+# for i in set(s):
+#     print(i, s.count(i))
+
 
 """ Remove all duplicates from a given string in Python """
 
+# s = "hi how are you hi how is your life"
+# res = ""
+# for word in s.split():
+#     if word not in res:
+#         res += " " + str(word)
+#
+# print(res)
+#
+# # method
+# print(set(s.split()))
+
 """ Python – Least Frequent Character in String """
+# s = "hi how are you"
+# for i in set(s):
+#     if s.count(i) == 1:
+#         print(i, end=" ")
+
 
 """ Python | Maximum frequency character in String """
 
+
 """ Python | Program to check if a string contains any special character """
+
 
 """ Generating random strings until a given string is generated """
 
+
 """ Find words which are greater than given length k """
 
-""" Python program for removing i-th character from a string
-Python program to split and join a string
-Python | Check if a given string is binary string or not
-Python program to find uncommon words from two Strings
-Python – Replace duplicate Occurrence in String
-Python – Replace multiple words with K
-Python | Permutation of a given string using inbuilt function
-Python | Check for URL in a String
-Execute a String of Code in Python
-String slicing in Python to rotate a string
-String slicing in Python to check if a string can become empty by recursive deletion
-Python Counter| Find all duplicate characters in string
-Python – Replace all occurrences of a substring in a string
-"""
+
+""" Python program for removing i-th character from a string"""
+
+
+"""Python program to split and join a string"""
+
+
+"""Python | Check if a given string is binary string or not"""
+
+
+"""Python program to find uncommon words from two Strings"""
+
+""" Python – Replace duplicate Occurrence in String"""
+
+"""Python – Replace multiple words with K"""
+
+"""Python | Permutation of a given string using inbuilt function """
+
+""" Python | Check for URL in a String """
+
+""" Execute a String of Code in Python"""
+
+"""String slicing in Python to rotate a string """
+
+""" String slicing in Python to check if a string can become empty by recursive deletion """
+
+""" Python Counter| Find all duplicate characters in string """
+
+""" Python – Replace all occurrences of a substring in a string """
