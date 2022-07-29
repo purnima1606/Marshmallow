834226e822ae2e270c449092e84c00955b248efe
purnima1606 <purnimasingh26th@gmail.com>
2022-07-30 01:10:43 +0530

    1st commit

---
+++ hacker_rank/set_introduction.py
@@ -0,0 +1,34 @@
+
+
+
+# n = int(input())
+# arr = []
+# for _ in range(n):
+#     a = int(input())
+#     arr.append(a)
+#
+# print(arr)
+
+# def average(array):
+#     res = set(array)
+#     sum_ = sum(res)
+#     average_ = sum_ / len(res)
+#     print(average_)
+#     # your code goes here
+#
+# n = int(input())
+# arr = []
+# for i in range(n):
+#     arr[i]= int(input())
+#
+#
+# average(arr)
+# if __name__ == '__main__':
+#  161 182 161 154 176 170 167 171 170 174
+
+
+
+n = int(input())
+s = set()
+for i in range(1,n+1):
+    s.add()
