132373e90b20c91687708005d88638dcace99c8a
purnima1606 <purnimasingh26th@gmail.com>
2022-08-04 16:08:26 +0530

    1st commit

---
+++ hacker_rank/basic_data_types/nested_list.py
@@ -0,0 +1,22 @@
+"""
+Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+Example
+
+records = [["chi", 20.0], ["beta", 50.0], ["alpha", 50.0]]
+
+The ordered list of scores is [20.0, 50.0], so the second lowest score is 50.0 .
+There are two students with that score: ["beta", "alpha"] . Ordered alphabetically, the names are printed as:
+
+alpha
+beta
+
+Input Format
+
+The first line contains an integer,N , the number of students.
+The 2N subsequent lines describe each student over 2 lines.
+- The first line contains a student's name.
+- The second line contains their grade.
+"""
